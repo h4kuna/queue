bb32fca0b35df8c2847a3b4a23ab6233a30a2a4d
Milan Matějček <milan.matejcek@gmail.com>
2020-08-09 05:30:07 +0200

    QueueFactory: without temp

--- src/QueueFactory.php
+++ src/QueueFactory.php
@@ -4,26 +4,10 @@ namespace h4kuna\Queue;
 
 final class QueueFactory
 {
-	/** @var string */
-	private $temp;
-
-
-	public function __construct(?string $temp = NULL)
-	{
-		if ($temp === NULL) {
-			$temp = sys_get_temp_dir();
-		}
-		$this->temp = $temp;
-	}
-
 
 	public function create(string $name, int $permission = 0666, int $maxMessageSize = Queue::MAX_MESSAGE_SIZE_NO_DEFINED): Queue
 	{
-		$file = $this->temp . DIRECTORY_SEPARATOR . $name;
-		touch($file);
-		$key = ftok($file, substr($name, 0, 1));
-
-		return new Queue($name, $key, $permission, $maxMessageSize);
+		return new Queue($name, crc32("$name.$permission.$maxMessageSize"), $permission, $maxMessageSize);
 	}
 
 }
