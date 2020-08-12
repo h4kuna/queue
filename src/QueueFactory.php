ab651ea921dc17635d2b8a67fa3ac1800589b6b0
Milan Matějček <milan.matejcek@gmail.com>
2020-08-12 12:21:07 +0200

    QueueFactory: default permission

--- src/QueueFactory.php
+++ src/QueueFactory.php
@@ -5,17 +5,24 @@ namespace h4kuna\Queue;
 final class QueueFactory
 {
 	/** @var int */
+	private $permission;
+
+	/** @var int */
 	private $messageSize;
 
 
-	public function __construct(int $messageSize = Queue::MAX_MESSAGE_SIZE)
+	public function __construct(int $permission = 0666, int $messageSize = Queue::MAX_MESSAGE_SIZE)
 	{
+		$this->permission = $permission;
 		$this->messageSize = $messageSize;
 	}
 
 
-	public function create(string $name, int $permission = 0666): Queue
+	public function create(string $name, int $permission = null): Queue
 	{
+		if ($permission === null) {
+			$permission = $this->permission;
+		}
 		return new Queue($name, crc32("$name.$permission.{$this->messageSize}"), $permission, $this->messageSize);
 	}
 
